d27434ee6d5e2610eec845272c3682660ac1781e
Britt Gresham <brittcgresham@gmail.com>
2014-07-30 15:45:15 +0000

    Example one complete

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "So much printing"
+puts "I'd much rather you 'not'."
+puts 'I "Said" do not touch this.'
