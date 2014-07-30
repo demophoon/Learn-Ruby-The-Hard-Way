c604ad63f6ddb2b28f929096d22b8aeb00c1abb1
Britt Gresham <brittcgresham@gmail.com>
2014-07-30 16:09:46 +0000

    Example five completed

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Britt Gresham'
+my_age = 21
+my_height = 76 # Inches
+my_weight = 165 # Pounds
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+puts "lets talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually, thats pretty light."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the taco bell."
+
+# Now we put it all together
+puts "If i add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
